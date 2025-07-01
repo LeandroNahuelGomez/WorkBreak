8b70490924499a2b7391a355e99c9e815d4a06ad
Leandro Gomez <leanngomezzz05@gmail.com>
2025-07-01 20:42:24 -0300

    Ultimos cambios en Routers

--- backend/src/routes/atributoProductoRoutes.js
+++ backend/src/routes/atributoProductoRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { obtenerAtributosProducto, crearAtributoProducto, actualizarAtributoProducto } = require("../controllers/atributoProductoController");
+const { obtenerAtributosProducto, crearAtributoProducto, actualizarAtributoProducto, eliminarAtributoProducto } = require("../controllers/atributoProductoController");
+const { checkAuth, checkRole } = require("../middlewares/authMiddleware");
 
 router.get("/", obtenerAtributosProducto);
-router.post("/", crearAtributoProducto);
-router.put("/:id", actualizarAtributoProducto);         
-
+router.post("/", checkAuth, checkRole([1,2]), crearAtributoProducto);
+router.put("/:id",checkAuth, checkRole([1,2]), actualizarAtributoProducto);         
+router.delete("/:id",checkAuth, checkRole([1,2]), eliminarAtributoProducto)
 
 module.exports = router;
